7f81976ed698d90933ee7435533b00e9128559d0
fadym <fadym@FADY>
2025-07-04 21:29:02 +0300

    Add project files.

---
+++ First Project/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace First_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine("25$ per small carpet ");
+            System.Console.WriteLine("35$ per large carpet");
+            int Numberofsmallcarpets = Convert.ToInt32(Console.ReadLine());
+            Numberofsmallcarpets = Numberofsmallcarpets * 25;
+            System.Console.WriteLine($"Price per small carpet {Numberofsmallcarpets}");
+            int Numberoflargecarpets = Convert.ToInt32(Console.ReadLine());
+            Numberoflargecarpets = Numberoflargecarpets * 35;
+            System.Console.WriteLine($"Price per large carpet {Numberoflargecarpets}");
+            int cost = Numberofsmallcarpets + Numberoflargecarpets;
+            System.Console.WriteLine($"cost {cost}\n");
+            double taxes;
+            taxes = 6.6;
+            double total; 
+            total = cost + taxes;
+            System.Console.WriteLine(total);
+            
+
+
+           
+            
+           
+        }
+    }
+}
